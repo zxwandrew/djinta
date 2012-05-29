cc6cde97d6a7207666e754c77502be3bd2c4a944
Andrew <zxwandrew@gmail.com>
2012-05-29 14:28:28 -0400

    index page created with favicon

--- djinta/urls.py
+++ djinta/urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),   
     url(r'^drawings/', include('drawings.urls')),
+    url(r'^$', 'drawings.views.create'),
 
 )
