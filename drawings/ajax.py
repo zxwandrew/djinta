7f4b8abacc5d1781f34cb3888515dd6a7bb27356
Andrew <zxwandrew@gmail.com>
2012-06-20 19:24:02 -0400

    answer is always  42

--- drawings/ajax.py
+++ drawings/ajax.py
@@ -1,4 +1,3 @@
-from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
 
 from django.http import HttpResponse, HttpResponseRedirect
@@ -9,6 +8,7 @@ from django.forms.models import modelformset_factory
 from django.core.context_processors import csrf
 from datetime import date, datetime
 from django.utils import simplejson
+from drawings.calc import *
 
 
 
@@ -19,8 +19,11 @@ def myexample(request, drawingpart):
 @dajaxice_register
 def send_form(request, form):
     #DP = form.cleaned_data.get('DP')
+    #temp = [{"type":"member","x1":88,"y1":105,"x2":205,"y2":235,"servx1":"5.867","servy1":"18.000","servx2":"13.667","servy2":"9.333","e":300000,"i":100,"area":10},{"type":"member","x1":205,"y1":235,"x2":311,"y2":175,"servx1":"13.667","servy1":"9.333","servx2":"20.733","servy2":"13.333","e":300000,"i":100,"area":10},{"type":"member","x1":311,"y1":175,"x2":431,"y2":283,"servx1":"20.733","servy1":"13.333","servx2":"28.733","servy2":"6.1"}]
     DP = simplejson.loads(form)
-    d2 = Drawing(drawingpart=DP, create_date=datetime.now())
-    d2.save()
+    temp = MainParse(DP)
+    #DP=[{"type":"member","e":300000,"i":100,"area":10}]
+    #d2 = Drawing(drawingpart=DP, create_date=datetime.now())
+    #d2.save()
 
-    return simplejson.dumps({'message':'You Sent%s!' % DP})
+    return simplejson.dumps({'message':'Answer is always: %s!' % temp})
--- drawings/calc.py
+++ drawings/calc.py
@@ -10,6 +10,10 @@ matplotlib.use('Agg')
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.dates import DateFormatter
+from scipy import *
+from numpy import *
+from sympy import *
+from django.utils import simplejson
 
 # file charts.py, a stupid little test to see if plot was working
 def simple(request):
@@ -30,3 +34,116 @@ def simple(request):
     response = django.http.HttpResponse(content_type='image/png')
     canvas.print_png(response)
     return response
+
+class member:
+    def __init__(self, x1t, y1t, x2t, y2t, it, et, areat):
+        self.x1=min(x1t,x2t)
+        self.y1=min(y1t, y2t)
+        self.x2=max(x2t, x1t)
+        self.y2=max(y2t, y1t)
+        self.i=it
+        self.e=et
+        self.area=areat
+        self.length = sqrt(abs(self.x1-self.x2)^2+abs(self.y1-self.y2)^2)
+    
+    def addjoint (self, jointt, x1t, y1t):
+        if(self.x1==x1t and self.y1==y1t):
+            self.joint1={'type': jointt}
+        elif(self.x2==x1t and self.y2==y1t):
+            self.joint2={'type': jointt}
+        #ignoring mid member joints for now
+
+class joint:
+    def __init__(self, x1t, y1t, typet):
+        self.x1=x1t
+        self.y1=y1t
+        self.type=typet
+class support:
+    magx=None
+    magy=None
+    def __init__(self, x1t, y1t, typet):
+        self.x1=x1t
+        self.y1=y1t
+        self.type=typet
+class force:
+    def __init__(self, x1t, y1t, typet, magnitudet):
+        self.x1=x1t
+        self.y1=y1t
+        self.type=typet
+        self.magnitude = magnitudet
+    def getx1(self):
+        return self.x1
+    def gettype(self):
+        return self.type
+    def gety1(self):
+        return self.y1
+    def getmagnitude(self):
+        return self.magnitude
+
+def FindReaction(AllParts, AllMembers, AllJoints, AllSupports, AllForces):
+    startx=AllJoints[0]['x1']
+    starty=AllJoints[1]['y1']
+    ZMoment=0
+    YMoment=0
+    xcount=0
+    ycount=0
+    for force in AllForces:
+        if(force.gettype()=='YForce'):
+            ZMoment+=(force.getx1()-startx)*force.magnitude()
+        if(force.gettype()=='XForce'):
+            YMoment+=(force.gety1()-starty)*force.magnitude()
+        if(force.gettype()=='MForce'):
+            ZMoment+=force.magnitude()
+    #Calc X forces from support
+    for support in AllSupports:
+        if (support.magx==None and support.type!='YSupport'):
+            tempname='x'+str(xcount)
+            x=Symbol(tempname)
+            YMoment+=(support.getx1()-startx)*x
+            xcount+=1
+        #Calc y forces from support
+        if (support.magy==None and support.type!='XSupport'):
+            tempname='y'+str(ycount)
+            y=Symbol(tempname)
+            ZMoment+=(support.getx1()-startx)*y
+            ycount+=1
+        answer=solve([ZMoment])
+        return answer['x1']
+    
+def MainParse(form):
+    '''
+    DPs = simplejson.loads(form)
+    AllParts=[]
+    AllMembers=[]
+    AllJoints=[]
+    AllSupports=[]
+    AllForces=[]
+    ObjNum=0
+    for x in DPs:
+        if(x['type']=='member'):
+            tempmember = member(x['servx1'], x['servy1'], x['servx2'], x['servy2'], x['i'], x['e'], x['area'])
+            AllParts.append(tempmember)
+            AllMembers.append(tempmember)
+        elif(x['type']=='Hinge' or x['type']=='FixedJoint'):
+            tempjoint = joint(x['servx1'], x['servy1'], x['type']);
+            AllParts.append(tempjoint)
+            AllJoints.append(tempjoint)
+        elif(x['type']=='XSupport' or x['type']=='YSupport' or x['type']=='PinSupport' or x['type']=='FixedSupport'):
+            tempsupport = joint(x['servx1'], x['servy1'], x['type']);
+            AllParts.append(tempsupport)
+            AllSupports.append(tempsupport)
+        elif(x['type']=='Xforce' or x['type']=='YForce' or x['type']=='MSupport' or x['type']=='DSupport'):
+            tempforce = joint(x['servx1'], x['servy1'], x['type'], x['magnitude']);
+            AllParts.append(tempforce)
+            AllForces.append(tempforce)
+        ObjNum+=1
+        '''
+    #answer=FindReaction(AllParts, AllMembers, AllJoints, AllSupports, AllForces)
+    
+    check=42
+    return check
+    
+
+
+
+
