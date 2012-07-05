bc669bc58413f375b0b7935851ae8a547f7a4900
Andrew Wang <zxwandrew@gmail.com>
2012-07-05 18:05:26 -0400

    ran into a little problem with what is the actual correct answer and some problem with sympy

--- drawings/calc.py
+++ drawings/calc.py
@@ -90,37 +90,154 @@ class force:
 def FindReaction(AllParts, AllMembers, AllJoints, AllSupports, AllForces):
     startx=AllSupports[0].x1
     starty=AllSupports[0].y1
-    ZMoment=0
-    YMoment=0
+    ZMoment=[]
+    YMoment=[]
+    TempZMoment=[]
+    TempYMoment=[]
+    SumX=[]
+    SumY=[]
+    
+    unknownx=[]
+    unknowny=[]
     xcount=0
     ycount=0
+    jointcount=1
+    #initiate z and y moment
+    
+    tempZM=0
+    tempYM=0
+    tempSumX=0
+    tempSumY=0
+    
+    
     for force in AllForces:
-        if(force.gettype()=='YForce'):
-            ZMoment+=(force.getx1()-startx)*force.getmagnitude()
         if(force.gettype()=='XForce'):
-            YMoment+=(force.gety1()-starty)*force.getmagnitude()
+            tempYM+=(force.gety1()-starty)*force.getmagnitude()
+            tempSumX+=force.getmagnitude()
+        if(force.gettype()=='YForce'):
+            tempZM+=(force.getx1()-startx)*force.getmagnitude()
+            tempSumY+=force.getmagnitude()
         if(force.gettype()=='MForce'):
-            ZMoment+=force.magnitude()
+            tempZM+=force.magnitude()
+    
     #Calc X forces from support
     #print(AllSupports)
     for support in AllSupports:
         if (support.magx==None and support.type!='YSupport'):
             tempname='x'+str(xcount)
-            x=Symbol(tempname)
-            YMoment+=(support.x1-startx)*x
+            unknownx.append(Symbol(tempname))
+            
+            SumX.append(1)
+            TempYMoment.append(support.y1-starty)
             xcount+=1
         #Calc y forces from support
         if (support.magy==None and support.type!='XSupport'):
             tempname='y'+str(ycount)
-            y=Symbol(tempname)
-            ZMoment+=(support.x1-startx)*y
-            ycount+=1
+            unknowny.append(Symbol(tempname))
             
-    print(ZMoment)
-    answer=solve([ZMoment])
-    print(answer)
-    return answer[y]
+            SumY.append(1)
+            TempZMoment.append(support.x1-startx)
+            ycount+=1
+    
+    TempYMoment.append((-1*tempYM))
+    TempZMoment.append((-1*tempZM))
+    SumX.append(-1*tempSumX)
+    SumY.append(-1*tempSumY)
+    
+    ZMoment.append(TempZMoment)
+    ZMoment.append(SumY)
+    
+    YMoment.append(TempYMoment)
+    YMoment.append(SumX)
+    
+     
+    for joint in AllJoints:
+        tempZM=0
+        tempYM=0
+        tempZM2=0
+        tempYM2=0
+        
+        tempSumX=0
+        tempSumY=0
+        TempZMoment=[]
+        TempYMoment=[]
+        
+        TempYMoment2=[]
+        TempZMoment2=[]
+    
+    
+        for force in AllForces:
+            if(force.gettype()=='XForce'):
+                if(force.gety1()>=joint.y1):
+                    tempYM+=(force.gety1()-joint.y1)*force.getmagnitude()
+                else:
+                    tempYM2+=(force.gety1()-joint.y1)*force.getmagnitude()
 
+            if(force.gettype()=='YForce'):
+                if(force.getx1()>=joint.x1):
+                    tempZM+=(force.getx1()-joint.x1)*force.getmagnitude()
+                else:
+                    tempZM2+=(force.getx1()-joint.x1)*force.getmagnitude()
+
+            if(force.gettype()=='MForce'):
+                if(force.getx1()>=joint.x1):
+                    tempZM+=force.magnitude()
+                else:
+                    tempZM2+=force.magnitude()
+        
+        #Calc X forces from support
+        #print(AllSupports)
+        for support in AllSupports:
+            if (support.magx==None and support.type!='YSupport'):
+                if(support.y1>=joint.y1):
+                    TempYMoment.append(support.y1-joint.y1)
+                    TempYMoment2.append(0)
+                else:
+                    TempYMoment2.append(support.y1-joint.y1)
+                    TempYMoment.append(0)
+                    
+                xcount+=1
+            #Calc y forces from support
+            if (support.magy==None and support.type!='XSupport'):
+                if(support.x1>=joint.x1):
+                    TempZMoment.append(support.x1-joint.x1)
+                    TempZMoment2.append(0)
+                else:
+                    TempZMoment2.append(support.x1-joint.x1)
+                    TempZMoment.append(0)
+                    
+                ycount+=1
+        
+        
+        
+        TempYMoment.append((-1*tempYM))
+        TempZMoment.append((-1*tempZM))
+        
+        TempYMoment2.append((-1*tempYM2))
+        TempZMoment2.append((-1*tempZM2))
+        
+        ZMoment.append(TempZMoment)        
+        YMoment.append(TempYMoment)
+        ZMoment.append(TempZMoment2)        
+        YMoment.append(TempYMoment2)
+        
+        jointcount+=1
+
+    #put zmoment into matrix format and solve
+    systemz = Matrix(ZMoment)
+    print(systemz)
+    print(unknowny)
+    answerx = solve_linear_system(systemz, *unknowny)
+    
+    #put ymoment into matrix format and solve
+    systemy = Matrix(YMoment)
+    print(systemy)
+    print(unknownx)
+    answery = solve_linear_system(systemy, *unknownx)
+    
+    #return answer
+    print(answerx)
+    return (answerx)
     
 def MainParse(form):
     
@@ -133,7 +250,7 @@ def MainParse(form):
     ObjNum=0
     
     #Will need to be deleted in the future
-    form=[{u'y2': 300, u'e': 300000, u'i': 100, u'area': 10, u'servy1': 25, u'servx1': 5, u'servx2': 20, u'x2': 300, u'servy2': 25, u'y1': 300, u'x1': 75, u'type': u'member'}, {u'y1': 300, u'servy1': 25, u'x1': 75, u'type': u'XSupport', u'servx1': 5}, {u'y1': 300, u'servy1': 25, u'x1': 300, u'type': u'PinSupport', u'servx1': 20}, {u'servy1': 25, u'servx1': 15, u'magnitude': u'-10', u'y1': 225, u'x1': 225, u'type': u'YForce'}]
+    form=[{u'y2': 300, u'e': 300000, u'i': 100, u'area': 10, u'servy1': 25, u'servx1': 5, u'servx2': 10, u'x2': 150, u'servy2': 25, u'y1': 300, u'x1': 75, u'type': u'member'}, {u'y2': 300, u'e': 300000, u'i': 100, u'area': 10, u'servy1': 25, u'servx1': 10, u'servx2': 20, u'x2': 300, u'servy2': 25, u'y1': 300, u'x1': 150, u'type': u'member'}, {u'y1': 300, u'servy1': 25, u'x1': 150, u'type': u'Hinge', u'servx1': 10}, {u'y1': 300, u'servy1': 25, u'x1': 75, u'type': u'YSupport', u'servx1': 5}, {u'y1': 300, u'servy1': 25, u'x1': 300, u'type': u'PinSupport', u'servx1': 20}, {u'y1': 300, u'servy1': 25, u'x1': 225, u'type': u'YSupport', u'servx1': 15}, {u'servy1': 25, u'servx1': 12, u'magnitude': u'-100', u'y1': 180, u'x1': 180, u'type': u'YForce'}]
     
     for x in form:
         print(x['type'])
