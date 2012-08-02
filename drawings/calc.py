06bd4205353cb03f672c0323acc8f42ceb720752
Andrew Wang <zxwandrew@gmail.com>
2012-08-02 01:35:30 -0400

    distributed force refined and included in reaction calculation

--- drawings/calc.py
+++ drawings/calc.py
@@ -17,6 +17,7 @@ os.environ['HOME'] = '/tmp'
 #from scipy import *
 #from numpy import *
 from sympy import *
+import math
 from django.utils import simplejson
 from decimal import Decimal
 from operator import itemgetter, attrgetter
@@ -42,7 +43,7 @@ def simple(request):
     return response
 
 class member:
-    def __init__(self, x1t, y1t, x2t, y2t, it, et, areat):
+    def __init__(self, x1t, y1t, x2t, y2t, it, et, areat,namet):
         self.x1=float(min(x1t,x2t))
         self.y1=float(min(y1t, y2t))
         self.x2=float(max(x2t, x1t))
@@ -51,6 +52,7 @@ class member:
         self.e=float(et)
         self.area=float(areat)
         self.length = sqrt(abs(self.x1-self.x2)**2+abs(self.y1-self.y2)**2)
+        self.name=namet
     
     def addjoint (self, jointt, x1t, y1t):
         if(self.x1==x1t and self.y1==y1t):
@@ -60,26 +62,29 @@ class member:
         #ignoring mid member joints for now
 
 class joint:
-    def __init__(self, x1t, y1t, typet):
+    def __init__(self, x1t, y1t, typet,namet):
         self.x1=float(x1t)
         self.y1=float(y1t)
         self.type=typet
+        self.name=namet
         
 class support:
     magx=None
     magy=None
-    def __init__(self, x1t, y1t, typet):
+    def __init__(self, x1t, y1t, typet,namet):
         self.x1=float(x1t)
         self.y1=float(y1t)
         self.type=typet
+        self.name=namet
         
         
 class force:
-    def __init__(self, x1t, y1t, typet, magnitudet):
+    def __init__(self, x1t, y1t, typet, magnitudet,namet):
         self.x1=float(x1t)
         self.y1=float(y1t)
         self.type=typet
         self.magnitude = float(magnitudet)
+        self.name=namet
     def getx1(self):
         return self.x1
     def gettype(self):
@@ -88,6 +93,24 @@ class force:
         return self.y1
     def getmagnitude(self):
         return self.magnitude
+
+class dforce:
+    def __init__(self, x1t, y1t, x2t,y2t, typet, f1t, f2t, directiont, onmembert, r1t, r2t, slopet, namet):
+        self.x1=float(x1t)
+        self.y1=float(y1t)
+        self.x2=float(x2t)
+        self.y2=float(y2t)
+        self.type=typet
+        self.f1 = float(f1t)
+        self.f2 = float(f2t)
+        self.r1= float(r1t)
+        self.r2= float(r2t)
+        self.direction = directiont
+        self.onmember=onmembert
+        self.slope=slopet
+        self.name=namet
+    def gettype(self):
+        return self.type
 
 def FindReaction(AllParts, AllMembers, AllJoints, AllSupports, AllForces):
     startx=AllSupports[0].x1
@@ -119,6 +142,41 @@ def FindReaction(AllParts, AllMembers, AllJoints, AllSupports, AllForces):
             SumY+=force.getmagnitude()
         if(force.gettype()=='MForce'):
             TempZMoment+=force.magnitude()
+        if(force.gettype()=='DForce'):
+            if(force.direction=="Global-Y"):
+                print(force.x1)
+                print(force.x2)
+                print((((force.x2-force.x1)/2+force.x1)-startx))
+                TempZMoment+=(((force.x2-force.x1)/2+force.x1)-startx)*((force.f1+force.f2)*(force.r2-force.r1)/2)
+                SumY+=(force.f1+force.f2)*(force.r2-force.r1)/2
+            elif(force.direction=="Local-Y"):
+                angle = math.atan(slope)
+                f1x = f1*math.sin(angle)
+                f1y = f1*math.cos(angle)
+                f2x = f2*math.sin(angle)
+                f2y = f2*math.cos(angle)
+                
+                TempZMoment+=((force.y1+((force.y2-force.y1)*(2*f1x+f2x)/(3*(f1x+f2x)))-starty)*((f1x+f2x)*(force.r2-force.r1)/2))
+                TempZMoment+=((force.x1+((force.x2-force.x1)*(2*f1y+f2y)/(3*(f1y+f2y)))-startx)*((f1y+f2y)*(force.r2-force.r1)/2))
+                SumX+=(force.f1x+force.f2x)*(force.r2-force.r1)/2
+                SumY+=(force.f1y+force.f2y)*(force.r2-force.r1)/2
+                
+                
+            elif(force.direction=="Global-X"):
+                TempZMoment+=((force.y2-force.y1)/2-starty)*((force.f1+force.f2)*(force.r2-force.r1)/2)
+                SumX+=(force.f1+force.f2)*(force.r2-force.r1)/2
+            elif(force.direction=="Local-X"):
+                angle = math.atan(slope)
+                f1x = f1*math.sin(angle)
+                f1y = f1*math.cos(angle)
+                f2x = f2*math.sin(angle)
+                f2y = f2*math.cos(angle)
+                
+                TempZMoment+=((force.y1+((force.y2-force.y1)*(2*f1x+f2x)/(3*(f1x+f2x)))-starty)*((f1x+f2x)*(force.r2-force.r1)/2))
+                TempZMoment+=((force.x1+((force.x2-force.x1)*(2*f1y+f2y)/(3*(f1y+f2y)))-startx)*((f1y+f2y)*(force.r2-force.r1)/2))
+                SumX+=(force.f1x+force.f2x)*(force.r2-force.r1)/2
+                SumY+=(force.f1y+force.f2y)*(force.r2-force.r1)/2
+                
     
     #Calc X forces from support
     #print(AllSupports)
@@ -144,20 +202,24 @@ def FindReaction(AllParts, AllMembers, AllJoints, AllSupports, AllForces):
             TempZMoment+=(support.x1-startx)*y
             ycount+=1
             allcount+=1
+        #Calc m forces
         if (support.type=='FixedSupport'):
             tempname='m'+str(mcount)
             m=Symbol(tempname)
-            unknown.append(m) 
+            unknown.append(m)
+            unknowny.append(m)
             
             TempZMoment+=m
+            SumY+=0*m
             mcount+=1
             allcount+=1
        
     ZMoment.append(TempZMoment)
     ZMoment.append(SumY)
-    
     #YMoment.append(TempYMoment)
     YMoment.append(SumX)
+    
+    print(ZMoment)
     
      
     for joint in AllJoints:
@@ -177,19 +239,58 @@ def FindReaction(AllParts, AllMembers, AllJoints, AllSupports, AllForces):
                 if(force.gety1()>=joint.y1):
                     TempZMoment+=(force.gety1()-joint.y1)*force.getmagnitude()
                 else:
-                    TempZMoment2+=(force.gety1()-joint.y1)*force.getmagnitude()
+                    #TempZMoment2+=(force.gety1()-joint.y1)*force.getmagnitude()
+                    TempZMoment+=(force.gety1()-joint.y1)*force.getmagnitude()
 
             if(force.gettype()=='YForce'):
                 if(force.getx1()>=joint.x1):
                     TempZMoment+=(force.getx1()-joint.x1)*force.getmagnitude()
                 else:
-                    TempZMoment2+=(force.getx1()-joint.x1)*force.getmagnitude()
+                    #TempZMoment2+=(force.getx1()-joint.x1)*force.getmagnitude()
+                    TempZMoment+=(force.getx1()-joint.x1)*force.getmagnitude()
 
             if(force.gettype()=='MForce'):
                 if(force.getx1()>=joint.x1):
                     TempZMoment+=force.magnitude()
                 else:
-                    TempZMoment2+=force.magnitude()
+                    #TempZMoment2+=force.magnitude()
+                    TempZMoment+=force.magnitude()
+            if(force.gettype()=='DForce'):
+                if(force.direction=="Global-Y"):
+                    TempZMoment+=((force.x2-force.x1)/2-startx)*((force.f1+force.f2)*(force.r2-force.r1)/2)
+                    SumY+=(force.f1+force.f2)*(force.r2-force.r1)/2
+                elif(force.direction=="Local-Y"):
+                    angle = math.atan(slope)
+                    f1x = f1*math.sin(angle)
+                    f1y = f1*math.cos(angle)
+                    f2x = f2*math.sin(angle)
+                    f2y = f2*math.cos(angle)
+                    
+                    TempZMoment+=((force.y1+((force.y2-force.y1)*(2*f1x+f2x)/(3*(f1x+f2x)))-starty)*((f1x+f2x)*(force.r2-force.r1)/2))
+                    TempZMoment+=((force.x1+((force.x2-force.x1)*(2*f1y+f2y)/(3*(f1y+f2y)))-startx)*((f1y+f2y)*(force.r2-force.r1)/2))
+                    SumX+=(force.f1x+force.f2x)*(force.r2-force.r1)/2
+                    SumY+=(force.f1y+force.f2y)*(force.r2-force.r1)/2
+                    
+                    
+                elif(force.direction=="Global-X"):
+                    TempZMoment+=((force.y2-force.y1)/2-starty)*((force.f1+force.f2)*(force.r2-force.r1)/2)
+                    SumX+=(force.f1+force.f2)*(force.r2-force.r1)/2
+                elif(force.direction=="Local-X"):
+                    angle = math.atan(slope)
+                    f1x = f1*math.sin(angle)
+                    f1y = f1*math.cos(angle)
+                    f2x = f2*math.sin(angle)
+                    f2y = f2*math.cos(angle)
+                    
+                    TempZMoment+=((force.y1+((force.y2-force.y1)*(2*f1x+f2x)/(3*(f1x+f2x)))-starty)*((f1x+f2x)*(force.r2-force.r1)/2))
+                    TempZMoment+=((force.x1+((force.x2-force.x1)*(2*f1y+f2y)/(3*(f1y+f2y)))-startx)*((f1y+f2y)*(force.r2-force.r1)/2))
+                    SumX+=(force.f1x+force.f2x)*(force.r2-force.r1)/2
+                    SumY+=(force.f1y+force.f2y)*(force.r2-force.r1)/2
+                    
+                    #force.y1+((force.y2-force.y1)*(2*force.f1x+force.f2x)/(3*(force.f1x+force.f2x)))
+                    
+                    #force.x1+((force.x2-force.x1)*(2*force.f1y+force.f2y)/(3*(force.f1y+force.f2y)))
+            
         
         #Calc X forces from support
         #print(AllSupports)
@@ -198,7 +299,9 @@ def FindReaction(AllParts, AllMembers, AllJoints, AllSupports, AllForces):
                 if(support.y1>=joint.y1):
                     TempZMoment+=(support.y1-joint.y1)*unknown[allcount]
                 else:
-                    TempZMoment2+=(support.y1-joint.y1)*unknown[allcount]
+                    #TempZMoment2+=(support.y1-joint.y1)*unknown[allcount]
+                    TempZMoment+=(support.y1-joint.y1)*unknown[allcount]
+                    
                 xcount+=1
                 allcount+=1
                 
@@ -207,11 +310,13 @@ def FindReaction(AllParts, AllMembers, AllJoints, AllSupports, AllForces):
                 if(support.x1>=joint.x1):
                     TempZMoment+=(support.x1-joint.x1)*unknown[allcount]
                 else:
-                    TempZMoment2+=(support.x1-joint.x1)*unknown[allcount]
+                    #TempZMoment2+=(support.x1-joint.x1)*unknown[allcount]
+                    TempZMoment+=(support.x1-joint.x1)*unknown[allcount]
+                    
                 ycount+=1
                 allcount+=1
             if(support.type=='FixedSupport'):
-                TempZMoment+=m
+                TempZMoment+=unknown[allcount]
                 mcount+=1
                 allcount+=1
                 
@@ -272,26 +377,35 @@ def MainParse(form):
     ObjNum=0
     
     #Will need to be deleted in the future
-    form=[{u'y2': 300, u'e': 300000, u'i': 100, u'area': 10, u'servy1': 25, u'servx1': 5, u'servx2': 10, u'x2': 150, u'servy2': 25, u'y1': 300, u'x1': 75, u'type': u'member'}, {u'y2': 300, u'e': 300000, u'i': 100, u'area': 10, u'servy1': 25, u'servx1': 10, u'servx2': 20, u'x2': 300, u'servy2': 25, u'y1': 300, u'x1': 150, u'type': u'member'}, {u'y1': 300, u'servy1': 25, u'x1': 150, u'type': u'Hinge', u'servx1': 10}, {u'y1': 300, u'servy1': 25, u'x1': 75, u'type': u'YSupport', u'servx1': 5}, {u'y1': 300, u'servy1': 25, u'x1': 300, u'type': u'PinSupport', u'servx1': 20}, {u'y1': 300, u'servy1': 25, u'x1': 225, u'type': u'YSupport', u'servx1': 15}, {u'servy1': 25, u'servx1': 12, u'magnitude': u'-100', u'y1': 180, u'x1': 180, u'type': u'YForce'}]
+    #form=[{u'y2': 300, u'e': 300000, u'i': 100, u'area': 10, u'servy1': 25, u'servx1': 5, u'servx2': 10, u'x2': 150, u'servy2': 25, u'y1': 300, u'x1': 75, u'type': u'member'}, {u'y2': 300, u'e': 300000, u'i': 100, u'area': 10, u'servy1': 25, u'servx1': 10, u'servx2': 20, u'x2': 300, u'servy2': 25, u'y1': 300, u'x1': 150, u'type': u'member'}, {u'y1': 300, u'servy1': 25, u'x1': 150, u'type': u'Hinge', u'servx1': 10}, {u'y1': 300, u'servy1': 25, u'x1': 75, u'type': u'YSupport', u'servx1': 5}, {u'y1': 300, u'servy1': 25, u'x1': 300, u'type': u'PinSupport', u'servx1': 20}, {u'y1': 300, u'servy1': 25, u'x1': 225, u'type': u'YSupport', u'servx1': 15}, {u'servy1': 25, u'servx1': 12, u'magnitude': u'-100', u'y1': 180, u'x1': 180, u'type': u'YForce'}]
+    #form=[{u'y2': 150, u'e': 300000, u'name': u'M0', u'area': 10, u'servy1': 15, u'i': 100, u'servx1': 10, u'servx2': 20, u'x2': 300, u'servy2': 15, u'y1': 150, u'x1': 150, u'type': u'member'}, {u'name': u'S1', u'servy1': 15, u'servx1': 10, u'y1': 150, u'x1': 150, u'type': u'FixedSupport'}, {u'servy1': 15, u'servx1': 12, u'magnitude': u'-100', u'y1': 180, u'x1': 180, u'type': u'YForce',  u'name': u'F2'}]
+    #fixed, 3 joints
+    #form=[{u'y2': 450, u'e': 300000, u'name': u'M0', u'area': 10, u'servy1': 15, u'i': 100, u'servx1': 5, u'servx2': 10, u'x2': 150, u'servy2': 15, u'y1': 450, u'x1': 75, u'type': u'member'}, {u'y2': 450, u'e': 300000, u'name': u'M1', u'area': 10, u'servy1': 15, u'i': 100, u'servx1': 10, u'servx2': 20, u'x2': 300, u'servy2': 15, u'y1': 450, u'x1': 150, u'type': u'member'}, {u'y2': 450, u'e': 300000, u'name': u'M2', u'area': 10, u'servy1': 15, u'i': 100, u'servx1': 20, u'servx2': 30, u'x2': 450, u'servy2': 15, u'y1': 450, u'x1': 300, u'type': u'member'}, {u'y2': 450, u'e': 300000, u'name': u'M3', u'area': 10, u'servy1': 15, u'i': 100, u'servx1': 30, u'servx2': 40, u'x2': 600, u'servy2': 15, u'y1': 450, u'x1': 450, u'type': u'member'}, {u'name': u'S4', u'servy1': 15, u'servx1': 5, u'y1': 450, u'x1':75, u'type': u'FixedSupport'}, {u'name': u'S5', u'servy1': 15, u'servx1': 40, u'y1': 450, u'x1': 600, u'type': u'FixedSupport'}, {u'name': u'P6', u'servy1': 15, u'servx1': 10, u'y1': 450, u'x1': 150, u'type': u'Hinge'}, {u'name': u'P7', u'servy1': 15, u'servx1': 20, u'y1': 450, u'x1': 300, u'type': u'Hinge'}, {u'name': u'P8', u'servy1': 15, u'servx1': 30, u'y1': 450, u'x1': 450, u'type': u'Hinge'}, {u'name': u'F9', u'servy1': 15, u'servx1': 15, u'magnitude': u'-100', u'y1': 225, u'x1': 225, u'type': u'YForce'}]
+    #fixed, distributed
+    form = [{u'y2': 375, u'e': 300000, u'name': u'M0', u'area': 10, u'servy1': 20, u'i': 100, u'servx1': 5, u'servx2': 25, u'x2': 375, u'servy2': 20, u'y1': 375, u'x1': 75, u'type': u'member'}, {u'slope': u'infinity', u'f1': u'2', u'f2': u'5', u'y2':0, u'r1': u'12', u'r2': u'17', u'servy1': 20, u'servx1': 17, u'servx2': 24.2, u'direction': u'Global-Y', u'x2': 1, u'servy2': 20, u'onmember': 0, u'y1': 0, u'x1': 0, u'type': u'DForce', u'name': u'D1'}, {u'name': u'S2', u'servy1': 20, u'servx1': 5, u'y1': 375, u'x1': 75, u'type': u'FixedSupport'}]
     
     for x in form:
         print(x['type'])
         if(x['type']=='member'):
-            tempmember = member(x['servx1'], x['servy1'], x['servx2'], x['servy2'], x['i'], x['e'], x['area'])
+            tempmember = member(x['servx1'], x['servy1'], x['servx2'], x['servy2'], x['i'], x['e'], x['area'], x['name'])
             AllParts.append(tempmember)
             AllMembers.append(tempmember)
         elif(x['type']=='Hinge' or x['type']=='FixedJoint'):
-            tempjoint = joint(x['servx1'], x['servy1'], x['type']);
+            tempjoint = joint(x['servx1'], x['servy1'], x['type'], x['name'])
             AllParts.append(tempjoint)
             AllJoints.append(tempjoint)
         elif(x['type']=='XSupport' or x['type']=='YSupport' or x['type']=='PinSupport' or x['type']=='FixedSupport'):
-            tempsupport = support(x['servx1'], x['servy1'], x['type']);
+            tempsupport = support(x['servx1'], x['servy1'], x['type'],x['name'])
             AllParts.append(tempsupport)
             AllSupports.append(tempsupport)
         elif(x['type']=='Xforce' or x['type']=='YForce' or x['type']=='MSupport' or x['type']=='DSupport'):
-            tempforce = force(x['servx1'], x['servy1'], x['type'], x['magnitude']);
+            tempforce = force(x['servx1'], x['servy1'], x['type'], x['magnitude'],x['name'])
             AllParts.append(tempforce)
             AllForces.append(tempforce)
+        elif(x['type']=='DForce'):
+            tempdforce = dforce(x['servx1'], x['servy1'],x['servx2'], x['servy2'], x['type'], x['f1'],x['f2'],x['direction'], x['onmember'], x['r1'], x['r2'], x['slope'], x['name'])
+            AllParts.append(tempdforce)
+            AllForces.append(tempdforce)
         ObjNum+=1
     
     sorted(AllParts, key=attrgetter('x1','y1'))
